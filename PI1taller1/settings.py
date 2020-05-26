47572e94d7ce33c85f6a84d7ef3c8c0d59af1756
Joab Romero <jjromeroh@eafit.edu.co>
2020-05-26 15:25:15 -0500

    "cambios para despliegue del back"

--- PI1taller1/settings.py
+++ PI1taller1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o!uf%@974@vzj*)+zxeo39^$+p_*18mn%y6549piu0nd!!i00f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
